11bec4b622f0baf59d6066f27187653e5fb27a77
Merholg <ixibitus@gmail.com>
2021-11-07 19:54:22 +0300

    Add implicit Intent

--- MyFirstApp/app/src/main/java/com/merholg/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/merholg/myfirstapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.merholg.myfirstapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 //private const val TAG = "MainActivity"
 private const val LIFE_CYCLE_LOG_TAG = "LifeCycleLog"
@@ -13,6 +17,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(LIFE_CYCLE_LOG_TAG, retString())
+
+        val poemButton: Button = findViewById(R.id.poemButton)
+        val poemLink = Uri.parse("https://rustih.ru/aleksandr-pushkin-burya/" )
+        val poemIntent = Intent(Intent.ACTION_VIEW, poemLink)
+        poemIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        if(poemIntent.resolveActivity(packageManager) != null) {
+            val chooser = Intent.createChooser(poemIntent, "@string/WhatSee")
+            poemButton.setOnClickListener {
+                startActivity(chooser)
+            }
+        }
+        else {
+            poemButton.isEnabled = false
+            Toast.makeText(this, "@string/NoActivityAppExist", Toast.LENGTH_LONG).show()
+        }
+
     }
 
     override fun onStart() {
