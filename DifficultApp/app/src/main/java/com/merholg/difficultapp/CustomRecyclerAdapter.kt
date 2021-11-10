08c20a3fd48223dd58e207e137f82f87c627cdfd
Merholg <ixibitus@gmail.com>
2021-11-10 15:27:21 +0300

    Proto complete

---
+++ DifficultApp/app/src/main/java/com/merholg/difficultapp/CustomRecyclerAdapter.kt
@@ -0,0 +1,35 @@
+package com.merholg.difficultapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomRecyclerAdapter(private val names: List<String>) :
+    RecyclerView.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var largeTextView: TextView? = null
+        var smallTextView: TextView? = null
+
+        init {
+            largeTextView = itemView.findViewById(R.id.textViewLarge) as TextView?
+            smallTextView = itemView.findViewById(R.id.textViewSmall) as TextView?
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.recyclerview_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.largeTextView?.text = names[position]
+        holder.smallTextView?.setText(R.string.town)
+    }
+
+    override fun getItemCount() = names.size
+}
--- DifficultApp/app/src/main/java/com/merholg/difficultapp/MainActivity.kt
+++ DifficultApp/app/src/main/java/com/merholg/difficultapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.merholg.difficultapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-// https://www.youtube.com/watch?v=cDF_yBCflXk
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 class MainActivity : AppCompatActivity() {
+
+//    private val names: List<String> = listOf()
+
+    private fun getTownList(): List<String> {
+        return this.resources.getStringArray(R.array.town_names).toList()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = CustomRecyclerAdapter(getTownList())
+
+
     }
 }
