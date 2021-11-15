cf39fdb982bb073128922ab40d8632a96d1fd67f
Merholg <ixibitus@gmail.com>
2021-11-15 20:31:51 +0300

    UnoFragment.kt filled

--- FinalExam/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ FinalExam/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
+// constant for the persistence key
+private const val LAST_SELECTED_ITEM = "LAST_SELECTED_ITEM"
 class MainActivity : AppCompatActivity() {
+
+    // early initialization of the bottom navigation bar
+    private lateinit var bottomNavigationMenu: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationMenu = findViewById(R.id.bottom_navigation_menu)
+
+        // setting clicks on elements of the bottom navigation bar
+        bottomNavigationMenu.setOnItemSelectedListener { item ->
+            var fragment: Fragment? = null
+            when(item.itemId) {
+                R.id.uno->{
+                    fragment = UnoFragment()
+                }
+                R.id.dos->{
+                    fragment = DosFragment()
+                }
+                R.id.tres->{
+                    fragment = TresFragment()
+                }
+            }
+            replaceFragment(fragment!!)
+            true
+        }
+
+        // restoring the state of the bottom navigation bar
+        // R.id.uno if not saved
+        bottomNavigationMenu.selectedItemId = savedInstanceState?.getInt(LAST_SELECTED_ITEM) ?: R.id.uno
+    }
+
+    // saving the state of the last clicked element of the bottom navigation bar
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(LAST_SELECTED_ITEM, bottomNavigationMenu.selectedItemId)
+        super.onSaveInstanceState(outState)
+    }
+
+    // function of replacing fragments with supportFragmentManager
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
---
+++ FinalExam/app/src/main/java/com/example/myapplication/UnoFragment.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
+
+// constant for the persistence key
+private const val LAST_SCALAR_VALUE = "LAST_SCALAR_VALUE"
+class UnoFragment : Fragment() {
+
+    // early initialization of the bottom navigation bar
+    private lateinit var unoResultTextView: TextView
+    private lateinit var unoRunButton: Button
+
+   override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       val view = inflater.inflate(R.layout.fragment_uno, container, false)
+       // only from view called findViewById in Fragments
+       unoResultTextView = view.findViewById(R.id.uno_result_text_view)
+       unoRunButton = view.findViewById(R.id.uno_run_button)
+
+       // load if state saved
+       if (savedInstanceState != null)
+           unoResultTextView.text = savedInstanceState.getString(LAST_SCALAR_VALUE)
+       // get new if no
+       else
+           scalar()
+       // get new if button pressed
+       unoRunButton.setOnClickListener {scalar()}
+       return view
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        // save current value
+        outState.putString(LAST_SCALAR_VALUE, unoResultTextView.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun scalar() {
+        unoResultTextView.text =  Random.nextInt(100).toString()
+    }
+ }
